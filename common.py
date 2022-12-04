656036c28771161a2831440268e8c4773b7afa77
Artem Zakharenko <zakhart1234567890@gmail.com>
2022-12-04 15:40:24 +0300

    Add train methods and common modules

---
+++ common.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+from train_methods import TrainingStats
+
+def plot_stats(lbfgs_stats: TrainingStats, 
+               adam_stats: TrainingStats,
+               name: str):
+    plt.plot([i for i in range(len(lbfgs_stats.loss))], lbfgs_stats.loss, label="lbfgs")
+    plt.plot([i for i in range(len(adam_stats.loss))], adam_stats.loss, label="adam")
+    plt.ylabel("loss")
+    plt.xlabel("function evaluations")
+    plt.yscale("log")
+    plt.title("loss plot")
+    plt.legend()
+    plt.savefig(fname=f"plots/loss/{name}.png")
+    plt.cla()
+
+    plt.plot([i for i in range(len(lbfgs_stats.grad_norm))], lbfgs_stats.grad_norm, label="lbfgs")
+    plt.plot([i for i in range(len(adam_stats.grad_norm))], adam_stats.grad_norm, label="adam")
+    plt.ylabel("grad norm")
+    plt.xlabel("function evaluations")
+    plt.yscale("log")
+    plt.title("grad norm plot")
+    plt.legend()
+    plt.savefig(fname=f"plots/grad/{name}.png")
+    plt.cla()
+
+    plt.plot([i for i in range(len(lbfgs_stats.train_accuracy))], lbfgs_stats.train_accuracy, label="lbfgs")
+    plt.plot([i for i in range(len(adam_stats.train_accuracy))], adam_stats.train_accuracy, label="adam")
+    plt.ylabel("train accuracy")
+    plt.xlabel("function evaluations")
+    plt.title("train accuracy plot")
+    plt.legend()
+    plt.savefig(fname=f"plots/accuracy/{name}.png")
+    plt.cla()
